80aec48e2962f46134c836da87d6e32c1496b98e
Кирилл Барабанщиков <lilkirill2020@gmail.com>
2024-02-27 14:12:44 +0400

    set landscape orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app_widget.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+
   runApp(const AppWidget());
 }
-
-
-
